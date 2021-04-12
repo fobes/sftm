87b937f2c498fc24f47a057c7767e46745e116fd
Fobes <fobes_misterio@mail.ru>
2021-04-12 22:56:12 +0300

    Fix number of workers

--- CTaskManager.cpp
+++ CTaskManager.cpp
@@ -34,7 +34,7 @@ bool CTaskManager::Start(unsigned short nNumberOfWorkers) noexcept
 	if (!m_workers[0].Init(this) || !TlsSetValue(this->m_nTlsWorker, this))
 		return false;
 
-	for (unsigned nWorker = 1; nWorker < m_nNumberOfWorkers; nWorker++)
+	for (unsigned nWorker = 1; nWorker <= m_nNumberOfWorkers; nWorker++)
 	{
 		if (!m_workers[nWorker].Start(this))
 			return false;
